329c5e495fe125d91b725755dfa8ecb2b0123768
johanpq <johanp.queiroz@gmail.com>
2026-03-17 19:57:20 -0300

    initial commit with configuration

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Press_Start_2P, Inter } from "next/font/google";
+import "./globals.css";
+
+const pressStart = Press_Start_2P({
+  variable: "--font-press_start",
+  subsets: ["latin"],
+  weight: "400"
+});
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: "400"
+});
+
+export const metadata: Metadata = {
+  title: "ECOP 2026 - Encontro de Computação do Oeste Potiguar",
+  description: "De 18 a 20 de Maio. Encontro de Computação do Oeste Potiguar. Descubra como se inscrever no mais inovador evento de tecnologia da região Oeste Potiguar.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`${pressStart.variable} ${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <main className="font-press_start text-3xl bg-tertiary">Rodando</main>
+  );
+}
