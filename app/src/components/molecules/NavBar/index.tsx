ee2c0fdaff2a1db745faeee8b874d0aaeb773da6
johanpq <johanp.queiroz@gmail.com>
2026-03-18 08:01:49 -0300

    added hover property and fixed margin to get at the center

--- app/src/components/molecules/NavBar/index.tsx
+++ app/src/components/molecules/NavBar/index.tsx
@@ -15,15 +15,15 @@ export default function NavBar() {
     const pathname = usePathname()
 
   return (
-    <header className="flex justify-center fixed top-8 w-full">
-        <nav className="flex justify-center items-center w-[46%] bg-white pt-4 pb-4 pl-8 pr-8 rounded-[50px]">
-            <ul className={`flex justify-center items-center w-full gap-8 list-none font-press_start text-[.7rem]`}>
+    <header className="fixed top-8 left-0 w-full flex justify-center">
+        <nav className="flex justify-center items-center max-w-full sm:max-w-125 md:max-w-137.5 xl:max-w-162.5 bg-white pt-4 pb-4 pl-8 pr-8 rounded-[50px]">
+            <ul className={`flex justify-center items-center w-full gap-2 xl:gap-8 list-none font-press_start text-[.5rem] sm:text-[.6rem] md:text-[.68rem] xl:text-[.7rem]`}>
                 {routes.map((el, index) => (
                     <li 
                     key={index}
-                    className={`${pathname === el.route ? 'border-3 border-secondary' : ''} p-1`} >
+                    className={`${pathname === el.route ? 'border-3 border-secondary' : ''} p-1 hover:scale-105`} >
                         <Link 
-                        
+                        className="hover:underline"
                         href={el.route} >
                             {el.name} 
                         </Link>
