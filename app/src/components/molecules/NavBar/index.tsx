1d39612bcea27c7f54a9875dac840f009e019729
johanpq <johanp.queiroz@gmail.com>
2026-03-18 10:40:12 -0300

    refactor: imported constants file where the routes are located

--- app/src/components/molecules/NavBar/index.tsx
+++ app/src/components/molecules/NavBar/index.tsx
@@ -1,15 +1,8 @@
 'use client'
 
+import { ROUTES } from "@/app/constants";
 import Link from "next/link"
 import { usePathname } from "next/navigation";
-
-const routes = [
-    { name: 'Início', route: '/' },
-    { name: 'Sobre', route: '/sobre' },
-    { name: 'Programação', route: '/programacao' },
-    { name: 'Eventos', route: '/eventos' },
-    { name: 'Artigos', route: '/artigos' }
-]
 
 export default function NavBar() {
     const pathname = usePathname()
@@ -18,12 +11,12 @@ export default function NavBar() {
     <header className="fixed top-8 left-0 w-full flex justify-center">
         <nav className="flex justify-center items-center max-w-full sm:max-w-125 md:max-w-137.5 xl:max-w-162.5 bg-white pt-4 pb-4 pl-8 pr-8 rounded-[50px]">
             <ul className={`flex justify-center items-center w-full gap-2 xl:gap-8 list-none font-press_start text-[.5rem] sm:text-[.6rem] md:text-[.68rem] xl:text-[.7rem]`}>
-                {routes.map((el, index) => (
+                {ROUTES.map((el, index) => (
                     <li 
                     key={index}
-                    className={`${pathname === el.route ? 'border-3 border-secondary' : ''} p-1 hover:scale-105`} >
+                    className={`${pathname === el.route ? 'border-3 border-secondary' : ''} p-1 `} >
                         <Link 
-                        className="hover:underline"
+                        className="inline-block transform hover:scale-105 hover:underline"
                         href={el.route} >
                             {el.name} 
                         </Link>
