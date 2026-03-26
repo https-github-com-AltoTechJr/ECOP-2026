c693e53cc74fa29a2f7a844012324e442634247f
tobias-fernandes <tobys312@gmail.com>
2026-03-26 13:50:05 -0300

    fix: remove unnecessary use client from Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-'use client'
-
 import NavBar from "./src/components/molecules/NavBar";
 import Sponsors from "./src/components/molecules/Sponsors";
 import TimerSection from "./src/components/organisms/Timer";
-    
+
 export default function Home() {
   return (
     <>
