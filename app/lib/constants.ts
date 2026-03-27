e0771e23c90d8ea9938cc4cedc4b4cc762268c3f
tobias-fernandes <tobys312@gmail.com>
2026-03-27 15:29:47 -0300

    fix: change route

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -2,10 +2,17 @@ import { IRoutes } from "./types";
 
 const ROUTES: IRoutes[] = [
     { name: 'Início', route: '/' },
+    /*
+    TODO: Change the routes to the correct ones when the pages are ready
     { name: 'Sobre', route: '/sobre' },
     { name: 'Programação', route: '/programacao' },
     { name: 'Eventos', route: '/eventos' },
     { name: 'Artigos', route: '/artigos' }
+    */
+    { name: 'Sobre', route: '/' },
+    { name: 'Programação', route: '/' },
+    { name: 'Eventos', route: '/' },
+    { name: 'Artigos', route: '/' }
 ]
 
 export { ROUTES }
