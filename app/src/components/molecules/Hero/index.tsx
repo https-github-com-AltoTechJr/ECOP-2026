472d0ee7ab0dcd659e87fbe5338860d70b022f2b
tobias-fernandes <tobys312@gmail.com>
2026-04-01 22:08:44 -0300

    refactor: move constants to its file

--- app/src/components/molecules/Hero/index.tsx
+++ app/src/components/molecules/Hero/index.tsx
@@ -3,54 +3,16 @@
 import Image from "next/image";
 import Button from "../../atoms/Button";
 import Link from "next/link";
+import { clouds } from "./constants";
 
 const Hero: React.FC = () => {
-
-    const clouds = [
-        {
-            src: "/first-cloud.svg",
-            alt: "Nuvem 1",
-            width: 180,
-            height: 90,
-            top: "12%",
-            duration: "19s",
-            delay: "-5s",
-        },
-        {
-            src: "/second-cloud.svg",
-            alt: "Nuvem 2",
-            width: 220,
-            height: 110,
-            top: "28%",
-            duration: "23s",
-            delay: "-1s",
-        },
-        {
-            src: "/third-cloud.svg",
-            alt: "Nuvem 3",
-            width: 140,
-            height: 70,
-            top: "45%",
-            duration: "26s",
-            delay: "-3s",
-        },
-        {
-            src: "/fourth-cloud.svg",
-            alt: "Nuvem 4",
-            width: 170,
-            height: 85,
-            top: "20%",
-            duration: "22s",
-            delay: "-7s",
-        },
-    ];
-
     return (
         <section className="relative w-full h-screen font-press_start flex items-center justify-center text-center overflow-hidden">
             <Image
                 src="/scene-no-cloud.png"
                 alt="Imagem de fundo da seção principal do site"
                 fill
+                unoptimized
                 priority
                 quality={100}
                 sizes="100vw"
