a5b39cfbad83a3872fffe15779d2f9c3e6fa14fa
tobias-fernandes <tobys312@gmail.com>
2026-05-12 19:22:55 -0300

    feat: add programming and navbar item

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,18 +1,19 @@
 import { IRoutes } from "./types";
 
 const ROUTES: IRoutes[] = [
-    { name: 'Início', route: '/' },
-    /*
-    TODO: Change the routes to the correct ones when the pages are ready
-    { name: 'Sobre', route: '/sobre' },
-    { name: 'Programação', route: '/programacao' },
-    { name: 'Eventos', route: '/eventos' },
-    { name: 'Artigos', route: '/artigos' }
-    */
-    { name: 'Sobre', route: '/sobre' },
-    { name: 'Programação', route: '/programacao' },
-    { name: 'Eventos', route: '/' },
-    { name: 'Artigos', route: 'https://periodicos.ufersa.edu.br/index.php/ecop/about/submissions' }
-]
+  { name: "Início", route: "/" },
+  { name: "Sobre", route: "/sobre" },
+  { name: "Programação", route: "/programacao" },
+  { name: "Eventos", route: "/" },
+  {
+    name: "Maratona de Programação",
+    route:
+      "https://docs.google.com/document/d/1KQ2JO64xvO2yZUDy3WFGZcJtznJAY7_le2S34osTdTE/edit?usp=sharing",
+  },
+  {
+    name: "Artigos",
+    route: "https://periodicos.ufersa.edu.br/index.php/ecop/about/submissions",
+  },
+];
 
-export { ROUTES }
+export { ROUTES };
